508a25531a9b546d1b3ad6c2dfc8a41d371d6c58
Aitor <45202115+Aitorlb7@users.noreply.github.com>
2020-11-26 12:40:47 +0100

    Behaviour bricks added & early Behaviour Tree

---
+++ Assets/OURTHINGS/BehaviourBricks/hasBullets.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+using Pada1.BBCore;
+using Pada1.BBCore.Framework;
+
+[Condition("MyConditions/Tank has ammo?")]
+[Help("Checks whether the tank has ammo.")]
+public class IsCopNear : ConditionBase
+{
+    public override bool Check()
+    {
+        return true;
+    }
+}
